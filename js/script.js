b9a3091c2d8d0d3e7eaf0efb41f4fcd97ebb9c98
SpGray1991 <91733893+SpGray1991@users.noreply.github.com>
2021-10-27 17:04:25 +0300

    v2

--- js/script.js
+++ js/script.js
@@ -1,29 +1,71 @@
- /* 1 */ 
- "use strict";
+" use strict ";
 
- let numberOfFilms = prompt (`Сколько фильмов вы уже посмотрели?`, ``);
+ let numberOfFilms;
+ function start() {
+    numberOfFilms = +prompt ( ` Сколько фильмов вы уже посмотрели?`, `` );
+    while (numberOfFilms == '' || numberOfFilms == null || isNaN(numberOfFilms)) {
+      numberOfFilms = +prompt ( ` Сколько фильмов вы уже посмотрели?`, `` );
+    }
+ }
 
+ start();
 
- /* 2 */
  let personalMovieDB = {
    count: numberOfFilms,
    movies: {},
    actors: {},
    genres: [],
    privat: false
+ };
+
+ function detectPesonalLevel() {
+      if (personalMovieDB.count<10) {
+        alert (`Просмотренно довольно мало фильмов`);
+      } else if ( personalMovieDB.count>=10 && personalMovieDB.count <=30 ) {
+          alert (`Вы классический зритель`);
+          } else if (personalMovieDB.count>30) {
+            alert (`Вы киноман`);
+            } else {
+              alert (`Произошла ошибка`);
+              }
+    }
+
+ detectPesonalLevel();
  
+ function rememberMyFilms() {
+      for (let i=0; i<2; i++) {
+        const a = prompt (`Один из последних просмотренных фильмов?`, ''),
+              b = prompt (`На сколько оцените его?`, '');
+        
+        if (a != '' && b != '' &&  a!=null &&  b!=null && a.length < 50 ) {
+          personalMovieDB.movies[a] = b;
+        } else {
+            alert('Error');
+            i--;
+        }
+      }
+   }
+
+ rememberMyFilms();
+
+ function showMyDB() {
+   if (personalMovieDB.privat==false) {
+     alert(1);
+   }
  }
+
+ showMyDB();
+
+ function writeYourGenres() {
+  for (let i=1; i<=3; i++) {
+    const genre = prompt (`Ваш любимый жанр под номером ${i}.`, '');
+        personalMovieDB.genres[i-1] = genre;
+      }   
+    }
+
+writeYourGenres();
+
+console.log (personalMovieDB);
  
  
- /* 3 */
- let c = prompt (`Один из последних просмотренных фильмов?`, ``);
- let d = prompt (`На сколько оцените его?`, ``);
- let a = prompt (`Один из последних просмотренных фильмов?`, ``);
- let b = prompt (`На сколько оцените его?`, ``);
- 
- personalMovieDB.movies[a] = b;
- personalMovieDB.movies[c] = d;
-   
- 
- 
- alert (personalMovieDB.movies)
+
